0e184087edcf826ef19dd29eb0d14b57ce6a158d
noratora <4438333+noratora@users.noreply.github.com>
2018-10-30 14:07:42 +0900

    写経 はじめての Laravel 第7章

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,49 @@
 |
 */
 
+use App\Task;
+use Illuminate\Http\Request;
+
+/**
+ * タスク一覧表示
+ */
 Route::get('/', function () {
-    return view('welcome');
+    // タスク検索処理
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255'
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+    }
+
+    // タスク作成処理
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * タスク削除
+ */
+Route::delete('/task/{task}', function (Task $task) {
+    // タスク削除処理
+    $task->delete();
+
+    return redirect('/');
 });
